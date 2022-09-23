92aa54af2559c0c2b40dfa5dc761ba6cf53c5cdc
hyunjoo <hjpark1397@naver.com>
2022-09-23 14:23:31 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/judy_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.judy_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/custom_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+// reference - nicholas.koo
+
+class CustomColor {//커스텀 컬러 template
+  //papaya app CustomColor값
+  static Color papayablue = const Color(0xff20aee5);
+  static Color papayapeach = const Color(0xffff8888);
+  static Color papayalightgrey = const Color(0xfff6f7f9);
+  static Color papayacharcoal = const Color(0xffbfbfbf);
+  static Color papayalightgreyaccent = const Color(0xffdedede);
+}
+
