3b3b0852a5d107f2ccef77a6e319e719eb59207c
hyunjoo <hjpark1397@naver.com>
2022-09-29 18:06:37 +0900

    Create wod_list.dart

---
+++ lib/user/wod_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class WodListPage extends StatefulWidget{
+  @override
+  _WodListPageState createState() => _WodListPageState();
+}
+class _WodListPageState extends State<WodListPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
