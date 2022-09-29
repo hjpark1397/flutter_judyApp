9cf07dbc62f460b598d06c7144de638efa0db075
hyunjoo <hjpark1397@naver.com>
2022-09-29 18:08:35 +0900

    마이페이지 파일 추가

---
+++ lib/user/user_mypage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+//마이페이지
+class UserMyPage extends StatelessWidget{
+  const UserMyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- lib/user/wod_list.dart
+++ lib/user/wod_list.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
+//로그인 후 내가 기록해둔 와드 리스트를 보여주는 페이지
 class WodListPage extends StatefulWidget{
+  const WodListPage({Key? key}) : super(key: key);
+
   @override
   _WodListPageState createState() => _WodListPageState();
 }
